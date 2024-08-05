25efa0595c03ecff1cf3818c548c689a5d695380
Leonardo <leonardoramosvallim@gmail.com>
2024-08-05 17:22:26 -0300

    improving homepage screen

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -8,20 +8,35 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Image.asset(
-          'assets/bebe_logo.png',
+          'assets/bebe.png',
           width: 50,
           height: 50,
         ),
+        backgroundColor: const Color.fromARGB(255, 236, 237, 242),
       ),
       body: Center(
         child: ConstrainedBox(
           constraints: const BoxConstraints.expand(),
-          child: const Text(
-            'Bem vindo!',
-            textAlign: TextAlign.center,
+          child: ListView(
+            scrollDirection: Axis.horizontal,
+            children: List.generate(10, (index) {
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text('Button ${index + 1}'),
+                ),
+              );
+            }),
           ),
         ),
       ),
     );
   }
+}
+
+void main() {
+  runApp(const MaterialApp(
+    home: HomePage(),
+  ));
 }
